03520e547901d2563576489720f6f279396fa9d2
warthundervlk <warthundervlk@gmail.com>
2023-02-20 14:20:06 -0300

    Setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import * as dotenv from 'dotenv' 
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('get')
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
